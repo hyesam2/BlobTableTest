2afee69d89aa5b390e9bdd3bc50fdd5c09d1f94c
Hyein <hyesamsam@gmail.com>
2021-07-14 14:27:05 +0900

    2nd

--- GetJSONData.cs
+++ GetJSONData.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.Storage.Blobs;
 
 namespace Hyein.Function
 {
@@ -16,12 +17,27 @@ namespace Hyein.Function
         public static string Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
         HttpRequest req, ILogger log, ExecutionContext context)
         {    
-
         string connStrA = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
         string requestBody = new StreamReader(req.Body).ReadToEnd();
         dynamic data = JsonConvert.DeserializeObject(requestBody);
         string valueA = data.a;
-            return valueA;
+
+        BlobServiceClient clientA = new BlobServiceClient(connStrA);
+        BlobContainerClient containerA = clientA.GetBlobContainerClient("hyeincon");
+        BlobClient blobA = containerA.GetBlobClient(valueA + ".json");
+
+        string responseA = "No Data";
+
+        if(blobA.Exists())
+        {
+            using (MemoryStream msA = new MemoryStream())
+            {
+                blobA.DownloadTo(msA);
+                responseA = System.Text.Encoding.UTF8.GetString(msA.ToArray());
+            }
+        }
+        
+        return responseA;
         }
     }
 }
